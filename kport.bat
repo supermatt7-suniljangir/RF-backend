3ea2e42d395451bbcd2dcf1056bbdf4a90e8a014
Sunil Jangir <matt348806@gmail.com>
2026-05-22 16:13:41 +0530

    latest fixes

--- kport.bat
+++
@@ -1,9 +0,0 @@
-
-text
-
-@echo off
-for /f "tokens=5" %%a in ('netstat -aon ^| find ":5500" ^| find "LISTENING"') do (
-    echo Killing PID %%a on port 5500
-    taskkill /PID %%a /F
-)
-echo Done.
